79b55f25d7fa90c5d726ee6b35e9fbd8932038b8
bryan <bryan@localhost>
2022-05-02 14:19:19 -0500

    organized menu code

--- Store Manager Tool/src/StoreManagerTool.java
+++ Store Manager Tool/src/StoreManagerTool.java
@@ -6,7 +6,6 @@
 // Copyleft, all wrongs reserved. 
 
 import java.awt.*;
-
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -14,17 +13,27 @@ import javax.swing.*;
 public class StoreManagerTool {
 	//Fixed 3/12/2022: Added story capacity to be more in line with original 
 	// Prototype with strings instead of an actual GUI
+	//********************************** Variables **********************************
 	public int storeCapacity = 200;
+	//********************************** END ariables **********************************
+	
+	//********************************** MAIN **********************************
 	public static void main (String [] args) {
+		//********************************** Title **********************************
 		JFrame display = new JFrame ("Store Manager");
 		display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//********************************** Login Flow **********************************
 		//TODO: Fix framing such that Username and Password are on different lines, with input box
 		JLabel StoreName = new JLabel ("Please Provide Credentials: \n Username: \n Password: \n");
 		StoreName.setPreferredSize(new Dimension (400, 600));
 		JMenuBar menubar = new JMenuBar();
 		display.getContentPane().add(StoreName, BorderLayout.NORTH);
+		
+		//********************************** Sub menus **********************************
 		JMenu menu1 = new JMenu ("User Mode");
 		JMenu menu2 = new JMenu ("About");
+		
 		// TODO: When run, have system load in shopper mode, using
 		// JMenu to switch to manager mode
 		JMenuItem Shopper = new JMenuItem("Shopper");
@@ -42,4 +51,6 @@ public class StoreManagerTool {
 		display.pack();
 		display.setVisible(true);
 	}
+	//********************************** END MAIN **********************************
+	
 }
