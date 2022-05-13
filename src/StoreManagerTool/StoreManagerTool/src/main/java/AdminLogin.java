9430b312e0cda1e26ca38e8ef56f57013dbec604
Santhoshini Arkatala <99048196+arkatala@users.noreply.github.com>
2022-05-13 00:48:07 -0500

    Add files via upload

---
+++ src/StoreManagerTool/StoreManagerTool/src/main/java/AdminLogin.java
@@ -0,0 +1,54 @@
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.connection.DatabaseConnection;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/AdminLogin")
+public class AdminLogin extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            //Getting all the parameters from the frontend (admin)
+            String email = request.getParameter("email");
+            String pass = request.getParameter("upass");
+
+            //Retriving our session
+            HttpSession hs = request.getSession();
+
+            //Calling Connection method
+            Connection con = DatabaseConnection.getConnection();
+
+            //Creating Statement
+            Statement st = con.createStatement();
+
+            //Querying inside the database
+            ResultSet resultset = st.executeQuery("select * from tbladmin where email='" + email + "' AND password='" + pass + "'");
+            //If all the details are correct
+            if (resultset.next()) {
+                hs.setAttribute("uname", resultset.getString("name"));
+                //Redirecting admin to dashboard page
+                response.sendRedirect("dashboard.jsp");
+
+            } else {
+                //If details are wrong
+                String message = "You have enter wrong credentials";
+                hs.setAttribute("credential", message);
+                //Redirecting admin to admin login page
+                response.sendRedirect("admin-login.jsp");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/StoreManagerTool/StoreManagerTool/src/main/java/Test.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+
+import com.connection.DatabaseConnection;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Connection con = DatabaseConnection.getConnection();
+		
+	}
+
+}
